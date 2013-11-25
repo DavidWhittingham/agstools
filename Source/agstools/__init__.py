b062bc160146cde9c3c91cbd8cfb085abf5b0063
David Whittingham <DavidWhittingham@users.noreply.github.com>
2013-11-25 17:48:34 +1000

    Increased version number for future development work.

--- Source/agstools/__init__.py
+++ Source/agstools/__init__.py
@@ -3,6 +3,6 @@
 Python Command-Line tool used to provide convenient access to administrative functions of ArcGIS Server 10.1+.
 """
 
-__version__   = "0.2.0"
+__version__   = "0.2.1"
 __author__    =  "David Whittingham"
 __copyright__ = "Copyright (C) 2013 David Whittingham"
