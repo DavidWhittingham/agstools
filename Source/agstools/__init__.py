fef3687a1875695cd0e607f7e204f5a183d2a932
David Whittingham <DavidWhittingham@users.noreply.github.com>
2015-01-20 15:04:27 +1000

    Bumped version number in anticipation of a future release.

--- Source/agstools/__init__.py
+++ Source/agstools/__init__.py
@@ -3,8 +3,8 @@
 Python Command-Line tool used to provide convenient access to administrative functions of ArcGIS Server 10.1+.
 """
 
-__version__   = "0.2.1"
+__version__   = "0.2.2"
 __author__    =  "David Whittingham"
-__copyright__ = "Copyright (C) 2014 David Whittingham"
+__copyright__ = "Copyright (C) 2015 David Whittingham"
 
 from agstools import *
