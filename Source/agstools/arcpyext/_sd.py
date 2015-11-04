e8c0332442c41390ddaa7941ef522a83d7d96286
David Whittingham <DavidWhittingham@users.noreply.github.com>
2015-11-04 12:01:36 +1000

    Fixed wrong variable name

--- Source/agstools/arcpyext/_sd.py
+++ Source/agstools/arcpyext/_sd.py
@@ -39,7 +39,7 @@ def sddraft_to_sd(sddraft, output = None, persist = False):
         sd_draft_backup_path = "{0}.sddraft.bak".format(path.splitext(sddraft)[0])
         copy2(sddraft, sd_draft_backup_path)
 
-    arcpyext.publishing.convert_service_draft_to_staged_service(sd_draft, output)
+    arcpyext.publishing.convert_service_draft_to_staged_service(sddraft, output)
 
     if persist == True:
         move(sd_draft_backup_path, sddraft)
